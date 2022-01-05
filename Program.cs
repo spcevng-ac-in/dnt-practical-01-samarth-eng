ff17e789fedf4ce0cc0ad0a545f0d90c788fb7a5
samarth-eng <gosaisamarth2000@gmail.com>
2022-01-05 08:45:47 +0530

    submit first practical

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, i;
+            Console.WriteLine("Enter the number");
+            number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                i = number % 10;
+                sum = sum + i;
+                number = number / 10;
+            }
+
+            Console.WriteLine(sum = sum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
